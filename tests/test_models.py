2efc9858a15abe902dbfc3e2b3251ab790117207
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-02-18 00:02:22 -0500

    created read and update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,34 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_read_a_product(self):
+        """It should read a product"""
+        product = ProductFactory()
+        logger.info(product)
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product_id)
+        found_product = Product.find(product_id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+
+   def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+
+        product.description = "This is a new discription"
+        original_id = product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "This is a new discription")
+
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "This is a new discription")
