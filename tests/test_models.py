3f93bd1f968c2002f5978d83abbfe81857f9df2e
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-02-18 22:31:16 -0500

    added new tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -136,3 +136,35 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "This is a new description")
+
+    def test_delete_a_product(self):
+        """It should delete a product"""
+        product = ProductFactory()
+        product.create()
+        self.assertEqual(len(Product.all()), 1)
+        product.delete()
+        self.assertEqual(len(Product.all()), 0)
+
+    def test_all_products(self):
+        """It should list all products in the database"""
+        products = Product.all()
+        self.assertEqual(len(products), 0)
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        products = product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_create_a_product_by_name(self):
+        """It should create a product by name"""
+        products = Product.all()
+        self.assertEqual(len(products), 0)
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
