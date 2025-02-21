69e27926253d4084a8aae495ff7cf84acb97b8ef
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-02-21 05:58:10 -0500

    created all test cases and code for the rest api

--- service/routes.py
+++ service/routes.py
@@ -18,6 +18,9 @@
 """
 Product Store Service with UI
 """
+from urllib.parse import quote_plus
+
+from service.models import Product, Category
 from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
 from service.models import Product
@@ -95,9 +98,9 @@ def create_products():
 ######################################################################
 
 @app.route("/products", methods=["GET"])
-def list_products():
-    """Returns a list of Products"""
-    app.logger.info("Request to list Products...")
+def list_all_products():
+    """Returns a list of all the products"""
+    app.logger.info("Request to list all products")
 
     products = Product.all()
 
@@ -158,27 +161,85 @@ def delete_products(product_id):
 # L I S T   B Y   N A M E
 ######################################################################
 
+@app.route("/products", methods=["GET"])
+def list_products_by_name():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
 
-#
-# PLACE YOUR CODE HERE
-#
+    products = []
+    name = request.args.get("name")
 
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
 
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
+    
 ######################################################################
 # L I S T   B Y   C A T E G O R Y
 ######################################################################
 
+@app.route("/products", methods=["GET"])
+def list_products_by_category():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
 
-#
-# PLACE YOUR CODE HERE
-#
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
 
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        # create enum from string
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # L I S T   B Y   A V A I L B I L I T Y
 ######################################################################
 
+@app.route("/products", methods=["GET"])
+def list_products_by_availability():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
 
-#
-# PLACE YOUR CODE HERE
-#
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        # create enum from string
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
